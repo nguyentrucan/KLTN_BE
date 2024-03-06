7691c4eb494bb257965443b5941a0dca1031dd12
Nguyễn Trúc An <114329768+nguyentrucan@users.noreply.github.com>
2024-03-06 14:40:28 +0700

    modified:   controller/blogCtrl.js 	modified:   routes/blogRoute.js

--- controller/blogCtrl.js
+++ controller/blogCtrl.js
@@ -48,4 +48,14 @@ const getAllBlogs = asyncHandler(async (req, res) => {
     }
 })
 
-module.exports = { createBlog, updateBlog, getBlog, getAllBlogs }
+const deleteBlog = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    try {
+        const deleteBlog = await Blog.findByIdAndDelete(id)
+        res.json(deleteBlog)
+    } catch (error) {
+        throw new Error(error);
+    }
+})
+
+module.exports = { createBlog, updateBlog, getBlog, getAllBlogs, deleteBlog }
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { createBlog, updateBlog, getBlog, getAllBlogs } = require('../controller/blogCtrl')
+const { createBlog, updateBlog, getBlog, getAllBlogs, deleteBlog } = require('../controller/blogCtrl')
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware')
 
 router.post('/', authMiddleware, isAdmin, createBlog)
 router.put('/:id', authMiddleware, isAdmin, updateBlog)
 router.get('/:id', getBlog)
 router.get('/', getAllBlogs)
+router.delete('/:id', authMiddleware, isAdmin, deleteBlog)
 
 module.exports = router
